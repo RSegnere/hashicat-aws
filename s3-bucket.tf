ea3b1ff4b2668b3f293d914ef1ff9e9d5cd898ce
Romain Ségneré-Yter <romain.segnere-yter@devoteam.com>
2023-08-15 13:03:21 +0000

    Added s3 bucket module 3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,14 +1,4 @@
 module "s3_bucket" {
   source  = "app.terraform.io/CHIP_test_organization/s3-bucket/aws//examples/complete"
   version = "2.8.0"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
 }
